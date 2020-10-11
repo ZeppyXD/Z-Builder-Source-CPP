0ee724ca0cef7ce5dd73f3a32f0f2e50db2cb4a6
Zephyr <71956077+ZephyrXD@users.noreply.github.com>
2020-10-11 16:40:31 -0400

    Add files via upload

--- Z-BuilderCL/Z-BuilderCL/Z-BuilderCL.cpp
+++ Z-BuilderCL/Z-BuilderCL/Z-BuilderCL.cpp
@@ -25,6 +25,15 @@ string decrypt2(string toEncrypt);
 // ID has to be 8 chars
 // ID has to be combined with HWID. Example: [ID{HWID}]
 
+std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
+	size_t start_pos = 0;
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+	}
+	return str;
+}
+
 int main(int argc, char* argv[])
 {
 	std::vector<string> CMDParts;
@@ -39,6 +48,7 @@ int main(int argc, char* argv[])
 		cout << "Command: ";
 		cin >> command;
 	}
+	command = ReplaceAll(command, "[SPACE]", " ");
 	string HWID = GetHWID();
 	string cmdSplitter = "==";
 	if (command.find(cmdSplitter) != string::npos)
